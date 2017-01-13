5350d9146c9caa6e2de0767ff486d6a9fa914993
Marlene Uwamahoro <uwamahoromarlene@gmail.com>
2017-01-13 12:30:14 +0200

    Separated files

--- food.rb
+++ food.rb
@@ -15,11 +15,4 @@ class Food
 
 end
 
-food1 = Food.new({name: "apple", color: "green", price: 300})
-food2 = Food.new({name: "tomato", color: "red", price: 50})
-food3 = Food.new({name: "flour", color: "white", price: 500})
-puts food1.info
-puts food1.price
-food1.price = 400
-puts food1.price
 
